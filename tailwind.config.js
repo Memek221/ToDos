6a1d118ba4d0f50c56d7447d9cb05cd2d904c8f4
Memek221 <przemek.batelt@gmail.com>
2023-11-11 17:44:43 +0100

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#0B0C14',
+        'background': '#EBEDF4',
+        'primary': '#2E3656',
+        'secondary': '#C0C5DD',
+        'accent': '#495588'
+      }
+    },
+    fontFamily: {
+      'sans': ['Work Sans', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
