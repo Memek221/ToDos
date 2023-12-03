e9623534943d414b9f15c088b12de87a93764002
Memek221 <przemek.batelt@gmail.com>
2023-12-03 12:24:31 +0100

    added supabase and render projects list by it

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import Projects from "./components/Projects"
 import ToDos from "./components/ToDos"
 
-import projects from './data/projects.json';
-
 function App() {
   return (
     <div className="flex flex-col h-screen bg-primary">
@@ -11,7 +9,7 @@ function App() {
         <h1 className="text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-accent to-secondary inline">ToDos</h1>
       </header>
       <div className="flex w-full h-5/6">
-        <Projects projects={projects}/>
+        <Projects/>
         <ToDos/>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,42 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Projects({projects}) {
-  const [projectsList, setProjectsList] = useState(projects)
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  'https://nhosxomvujuwxmoutreu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ob3N4b212dWp1d3htb3V0cmV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1OTk3OTksImV4cCI6MjAxNzE3NTc5OX0.aZZhqp5QRAGXHYqzjj37UN678d2s5SrER6ApO67a5Uo'
+);
+
+export default function Projects() {
+  const [projectsList, setProjectsList] = useState([]);
+
+  console.log(projectsList);
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  async function getProjects() {
+    const { data } = await supabase.from('Projects').select();
+    setProjectsList(data);
+  }
 
   return (
     <section className="w-1/5 flex flex-col gap-4 px-2 py-5">
       <button className="bg-accent text-text rounded cursor-pointer h-20 flex items-center justify-center">
         Creat New Project +
       </button>
-      <div className='overflow-auto border-2  border-accent rounded p-2'>
+      <div className="overflow-auto border-2  border-accent rounded p-2">
         <ul>
-          {projectsList.projects.map((project, id) => {
+          {projectsList.map(project => {
             return (
               <li
-                key={id}
+                key={project.ProjectID}
                 className="bg-secondary my-2 text-text rounded cursor-pointer h-20 flex items-center justify-center"
               >
-                <span>{project.name}</span>
+                <button className='w-full h-full'>
+                  <span>{project.Name}</span>
+                </button>
               </li>
             );
           })}
